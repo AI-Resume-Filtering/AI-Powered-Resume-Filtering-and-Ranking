ba1ece524dcc2aad0f502a6a76f0c97f06bb17a1
Mayur Ughade <ughademayur67@gemail.com>
2026-02-16 15:18:09 +0530

    simple change

--- Resume_Parser/__init__.py
+++ Resume_Parser/__init__.py
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-=======
-"""
-Resume Parser Package
-Parses PDF/DOCX resumes and job descriptions to text
 
-Author: Mahesh Nikas
-Version: 1.0.0
-"""
 
 from .resume_parser import ResumeParser
 from .batch_parser import BatchParser
@@ -20,4 +12,3 @@ __version__ = '1.0.0'
 __author__ = 'V. T'
 
 print("✓ Resume Parser Package Loaded")
->>>>>>> 8cc503a6d053f691a1fd8637453a6bf6b418db13
